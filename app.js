699c42c658ed3f7a9d09e7d5149ae37c262bea8e
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-07-02 23:56:34 +0200

    game logic and basic UI finished

---
+++ app.js
@@ -0,0 +1,60 @@
+const PLAYER_1 = "fa-circle-o";
+const PLAYER_2 = "fa-times";
+let round = 1;
+
+const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+];
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const boxes = Array.from(document.querySelectorAll(".box"));
+boxes.forEach((box) => box.addEventListener("click", select));
+
+function select(evt) {
+    // const { row, column } = evt.target.dataset;
+    const row = parseInt(evt.target.dataset.row, 10);
+    const column = parseInt(evt.target.dataset.column, 10);
+    const turn = round % 2 === 0 ? PLAYER_2 : PLAYER_1;
+    if (board[row][column] !== "") return;
+
+    evt.target.classList.add(turn);
+    board[row][column] = turn;
+    round++;
+
+    console.log(check());
+}
+console.log("round", round);
+
+function check() {
+    const result = board.reduce((total, row) => total.concat(row));
+    let winner = null;
+    let moves = {
+        "fa-times": [],
+        "fa-circle-o": [],
+    };
+    result.forEach((field, index) =>
+        moves[field] ? moves[field].push(index) : null
+    );
+    winningCombinations.forEach((combination) => {
+        if (combination.every((index) => moves[PLAYER_1].indexOf(index) > -1)) {
+            winner = "Winner: Player 1";
+        }
+        if (combination.every((index) => moves[PLAYER_2].indexOf(index) > -1)) {
+            winner = "Winner: Player 2";
+        }
+    });
+
+    return winner;
+}
