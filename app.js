85659b1d51a884983d32297b71788be240c49d8b
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-07-03 18:56:14 +0200

    game UI finished

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const gameBoard = document.querySelector(".board");
+const playAgainButton = document.querySelector(".hidden");
 const PLAYER_1 = "fa-circle-o";
 const PLAYER_2 = "fa-times";
 let round = 1;
 
-const board = [
+const gameMatrix = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
@@ -22,39 +24,65 @@ const winningCombinations = [
 const boxes = Array.from(document.querySelectorAll(".box"));
 boxes.forEach((box) => box.addEventListener("click", select));
 
+playAgainButton.addEventListener("click", restartGame);
+
 function select(evt) {
     // const { row, column } = evt.target.dataset;
     const row = parseInt(evt.target.dataset.row, 10);
     const column = parseInt(evt.target.dataset.column, 10);
     const turn = round % 2 === 0 ? PLAYER_2 : PLAYER_1;
-    if (board[row][column] !== "") return;
+    if (gameMatrix[row][column] !== "") return;
 
     evt.target.classList.add(turn);
-    board[row][column] = turn;
+    gameMatrix[row][column] = turn;
     round++;
 
     console.log(check());
 }
-console.log("round", round);
+
+function blockBoardAfterGameFinish() {
+    boxes.forEach((box) => box.removeEventListener("click", select));
+}
+
+function restartGame() {
+    location.reload();
+}
 
 function check() {
-    const result = board.reduce((total, row) => total.concat(row));
+    // const result = board.reduce((total, row) => total.concat(row));
+    const result = gameMatrix.flat();
+
     let winner = null;
     let moves = {
         "fa-times": [],
         "fa-circle-o": [],
     };
+
     result.forEach((field, index) =>
         moves[field] ? moves[field].push(index) : null
     );
+
+    console.log("result: " + result);
+
     winningCombinations.forEach((combination) => {
         if (combination.every((index) => moves[PLAYER_1].indexOf(index) > -1)) {
             winner = "Winner: Player 1";
+            combination.forEach((index) => {
+                boxes[index].classList.add("winning-field");
+            });
         }
         if (combination.every((index) => moves[PLAYER_2].indexOf(index) > -1)) {
             winner = "Winner: Player 2";
+            combination.forEach((index) => {
+                boxes[index].classList.add("winning-field");
+            });
         }
     });
 
+    if (winner) {
+        gameBoard.classList.add("game-finished");
+        playAgainButton.classList.remove("hidden");
+        blockBoardAfterGameFinish();
+    }
     return winner;
 }
